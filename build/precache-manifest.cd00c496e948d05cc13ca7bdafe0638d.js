a60a2e76bf565fa433eff665c379018bbc050c7c
Shabarirajan07 <shabarirajan77@gmail.com>
2024-10-07 23:27:44 +0530

    .

---
+++ build/precache-manifest.cd00c496e948d05cc13ca7bdafe0638d.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1443699b8b2be8530bc6d0ea39a7ceb2",
+    "url": "/real-time-stock-market-dashboard/index.html"
+  },
+  {
+    "revision": "7a651e232d3e1b33fffa",
+    "url": "/real-time-stock-market-dashboard/static/css/2.5442e9d3.chunk.css"
+  },
+  {
+    "revision": "b2d9faa1c20fefa23082",
+    "url": "/real-time-stock-market-dashboard/static/css/main.99c2ed7d.chunk.css"
+  },
+  {
+    "revision": "7a651e232d3e1b33fffa",
+    "url": "/real-time-stock-market-dashboard/static/js/2.ce35236d.chunk.js"
+  },
+  {
+    "revision": "e7eae68caf8432d07f7e41ebfb0ef5d0",
+    "url": "/real-time-stock-market-dashboard/static/js/2.ce35236d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b2d9faa1c20fefa23082",
+    "url": "/real-time-stock-market-dashboard/static/js/main.42e231af.chunk.js"
+  },
+  {
+    "revision": "fe743220eb0b56934775",
+    "url": "/real-time-stock-market-dashboard/static/js/runtime-main.ce889a01.js"
+  }
+]);
